e4d9fbc8f2b8182ee6ec06dba0177d82f95cc5d6
liutao <encino@163.com>
2017-02-22 23:23:05 +0800

    jdk 1.8 实现类不需overide

--- src/main/java/com/liutao/proxy/MyInvocationHandler.java
+++ src/main/java/com/liutao/proxy/MyInvocationHandler.java
@@ -12,7 +12,6 @@ public class MyInvocationHandler implements InvocationHandler{
     public MyInvocationHandler(Object target) {
         this.target=target;
     }
-    @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("method :"+ method.getName()+" is invoked!");
         return method.invoke(target,args);
