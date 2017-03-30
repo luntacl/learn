ebcbf211f52af5d8b2687d43ae2fc17c8aa638e2
liutao <encino@163.com>
2017-03-30 20:29:13 +0800

    maven dependency

---
+++ src/test/java/com/liutao/whatever/JodaTimeTest.java
@@ -0,0 +1,22 @@
+package com.liutao.whatever;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+/**
+ * Created by liutao on 2017/3/30.
+ */
+public class JodaTimeTest {
+
+    @Test
+    public void test1() {
+        DateTime time = new DateTime();
+        System.out.println(time);
+//        String timeStr = "2016-03-30 17:53:23";
+        System.out.println(time.toString("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(time.toDate());
+        DateTime.Property property = DateTime.now().millisOfDay();
+        System.out.println(property.get());
+
+    }
+}
