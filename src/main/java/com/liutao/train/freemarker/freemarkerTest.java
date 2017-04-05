73c9322db9802947f27b62a18802b8772d4d79f0
liutao <encino@163.com>
2017-04-06 00:02:55 +0800

    freemarke使用

--- src/main/java/com/liutao/train/freemarker/freemarkerTest.java
+++ src/main/java/com/liutao/train/freemarker/freemarkerTest.java
@@ -1,5 +1,6 @@
 package com.liutao.train.freemarker;
 
+import com.alibaba.fastjson.JSONObject;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
@@ -15,12 +16,17 @@ import java.util.Map;
 public class freemarkerTest {
 
     public static void main(String[] args) throws IOException, TemplateException {
-        Template t = new Template(null, new StringReader("用户名：${user};URL：    ${url};姓名： 　${name}, ${method.getName(\"2\")}"), null);
+        Template t = new Template(null, new StringReader("用户名：${user};URL：    ${url};姓名： <#assign text=\"${users}\" /> <#assign data=text?eval />　${data}, ${method.getName(\"2\")}"), null);
         //创建插值的Map
         Map<String, Object> map = new HashMap<String,Object>();
         map.put("user", "lavasoft");
         map.put("url", "http://www.baidu.com/");
         map.put("name", "百度");
+        JSONObject json = new JSONObject();
+        JSONObject user = new JSONObject();
+        user.put("name", "zhangsan");
+        user.put("age",18);
+        json.put("users", user.toJSONString());
         map.put("method", new freemarkerTest());
         //执行插值，并输出到指定的输出流中
         t.process(map, new OutputStreamWriter(System.out));
